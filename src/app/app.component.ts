6e7b0fc084bed7bc33895b6fa18e77246bcebc76
gbesset <guillaume.besset.pro@gmail.com>
2018-04-17 16:40:19 +0200

    RXJS  Mise en place Observable et Observer (seconds)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+	connectedFromSeconds: number;
+
 	constructor() {} 
  
+   ngOnInit(){
+   	  //  crée un observale qui émet un chiffre toutes les secondes
+   	  const counter = Observable.interval(1000);
+
+
+   	  //observer le compteur
+   	  //Subscribe permet observer un observable et réagir a ses changements
+   	  counter.subscribe(
+   	  	//changement valeur (recoit les données)
+   	  	(value: number) => {
+   	  		this.connectedFromSeconds = value;
+   	  	},
+   	  	//erreur emise par l'observable
+   	  	(error: any) => {
+   	  		console.log('Erreur rencontrée par l\'observable');
+   	  	},
+   	  	//si observale se complete
+
+   	  	() => {
+   	  		console.log('Observale completé');
+   	  	}
+   	  )
+   }
 }
